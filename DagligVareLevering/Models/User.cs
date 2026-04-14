a7f54980ea18b8e9473023d2fd0e0038361aa2a2
RasmusMV <rav001@edu.zealand.dk>
2026-04-14 23:50:14 +0200

    Jeg har lavet en mappe "Models" og lavet et udkast til klasserne Order, Product, Store og User

---
+++ DagligVareLevering/Models/User.cs
@@ -0,0 +1,33 @@
+﻿namespace DagligVareLevering.Models
+{
+    public class User
+    {
+
+        public User() { }
+
+        public User(string name, string adress, string phonenumber, string email)
+        {
+            Name = name;
+            Adress = adress;
+            Phonenumber = phonenumber;  
+            Email = email;
+        }
+
+        public int UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Adress { get; set; }
+
+        public string Phonenumber { get; set; }
+
+        public string Email { get; set; }
+
+        public virtual ICollection<Product> Basket { get; set; }
+
+        public virtual ICollection<Order> OrderHistory { get; set; }
+
+
+
+    }
+}
