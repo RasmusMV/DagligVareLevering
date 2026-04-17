8fb848b7f63413c875e0b56995208ff3ec207832
lbstok <stoklas0201@gmail.com>
2026-04-17 10:50:45 +0200

    tilføjet cards og style

---
+++ DagligVareLevering/Pages/Groceries.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DagligVareLevering.Pages
+{
+    public class GroceriesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
