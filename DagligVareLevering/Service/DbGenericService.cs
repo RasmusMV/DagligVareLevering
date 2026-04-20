8746279a91217f924a3536f3bb13b9e6e7b799b4
RasmusMV <rav001@edu.zealand.dk>
2026-04-21 00:31:05 +0200

    Made a number of service classes and implemented basic crud operations in DbGenericService and added dependency injections to every model for DbGenericService. also added interfaces with some simple examples of things to be implemented.

---
+++ DagligVareLevering/Service/DbGenericService.cs
@@ -0,0 +1,43 @@
+﻿using DagligVareLevering.EFDbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace DagligVareLevering.Service
+{
+    public class DbGenericService<T> : IService<T> where T : class
+    {
+        private readonly AppDbContext _context;
+
+        public DbGenericService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddObjectAsync(T obj)
+        {
+            await _context.Set<T>().AddAsync(obj);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteObjectAsync(T obj)
+        {
+            _context.Set<T>().Remove(obj);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<T> GetObjectByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetObjectsAsync()
+        {
+            return await _context.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task UpdateObjectAsync(T obj)
+        {
+            _context.Set<T>().Update(obj);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ DagligVareLevering/Service/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using DagligVareLevering.Models;
+
+namespace DagligVareLevering.Service
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<Order>> SortById();
+        Task<IEnumerable<Order>> SortByIdDescending();
+        Task<IEnumerable<Order>> SortByTotalPrice();
+
+    }
+}
