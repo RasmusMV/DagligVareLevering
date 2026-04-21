aa80afb0d85cbaabb9e7613a20908c339c1a1d4f
SofieKnap <sok013@edu.zealand.dk>
2026-04-21 15:00:57 +0200

    Opdateret deliveryTime, nu med kalender på

--- DagligVareLevering/Migrations/20260421111936_AddDeliveryIntervalToOrder.Designer.cs
+++ DagligVareLevering/Migrations/20260421111936_AddDeliveryIntervalToOrder.Designer.cs
@@ -4,6 +4,7 @@ using DagligVareLevering.EFDbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DagligVareLevering.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260421111936_AddDeliveryIntervalToOrder")]
+    partial class AddDeliveryIntervalToOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
