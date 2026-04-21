ec45f411370fd6a5325092a081defe3ce682625e
lbstok <stoklas0201@gmail.com>
2026-04-21 22:26:55 +0200

    tried to make the cards organized and no duplicates, still need some work

--- DagligVareLevering/Pages/Groceries.cshtml.cs
+++ DagligVareLevering/Pages/Groceries.cshtml.cs
@@ -1,12 +1,35 @@
+using DagligVareLevering.EFDbContext;
+using DagligVareLevering.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
 
 namespace DagligVareLevering.Pages
 {
     public class GroceriesModel : PageModel
     {
-        public void OnGet()
+
+        private readonly AppDbContext _context;
+
+        public GroceriesModel(AppDbContext context)
         {
+            _context = context;
+        }
+        public Product? SelectedProduct { get; set; }
+
+        public IList<IGrouping<string, Product>> GroupedProducts { get; set; }
+
+        public void OnGet(int? id)
+        {
+            var products = _context.Products.OrderBy(p => p.Name).ThenBy(p => p.Price).ToList();
+            GroupedProducts = products.GroupBy(p => p.Name).ToList();
+
+            if (id != null)
+            {
+                SelectedProduct = products
+                    .FirstOrDefault(p => p.ProductId == id);
+            }
         }
     }
 }
