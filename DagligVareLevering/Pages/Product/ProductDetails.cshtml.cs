961418f9169c4182eb4d2077821b3b20a2072f73
lbstok <stoklas0201@gmail.com>
2026-05-12 10:25:50 +0200

    added product detail cs

--- DagligVareLevering/Pages/Product/ProductDetails.cshtml.cs
+++ DagligVareLevering/Pages/Product/ProductDetails.cshtml.cs
@@ -1,3 +1,5 @@
+using DagligVareLevering.Models;
+using DagligVareLevering.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,19 @@ namespace DagligVareLevering.Pages.Product
 {
     public class ProductDetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IService<Models.Product> _productService;
+
+        public ProductDetailsModel(IService<Models.Product> productService)
         {
+            _productService = productService;
+        }
+
+        public Models.Product? Product { get; set; }
+
+        public async Task OnGetAsync(int id)
+        {
+            var products = await _productService.GetObjectsAsync();
+            Product = products.FirstOrDefault(p => p.ProductId == id);
         }
     }
 }
