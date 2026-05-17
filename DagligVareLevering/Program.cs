a1ef12f5a13a61a00ce94554ae45dec21d3a97c6
SofieKnap <sok013@edu.zealand.dk>
2026-05-17 22:09:03 +0200

    .

--- DagligVareLevering/Program.cs
+++ DagligVareLevering/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddScoped<IService<User>, DbGenericService<User>>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<CartEventService>();
+
 
 
 
