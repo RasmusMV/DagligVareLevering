90f67e526dedadc9a6e2ec0b331935c57b5bfd7c
lbstok <stoklas0201@gmail.com>
2026-05-18 09:50:25 +0200

    unittest

--- DagligVareLeveringTest/ProductTest.cs
+++ DagligVareLeveringTest/ProductTest.cs
@@ -1,10 +1,43 @@
 namespace DagligVareLeveringTest;
+using DagligVareLevering.Models;
+using DagligVareLevering.Pages.Product;
+using DagligVareLevering.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
 
 [TestClass]
 public class ProductTest
 {
-    [TestMethod]
-    public void TestMethod1()
+    [TestClass]
+    public class CartTest
     {
+        [TestMethod]
+        public async Task OnPostAddToCartAsync_UserNotLoggedIn_RedirectsToLogin()
+        {
+            // Arrange
+            var model = new GroceriesModel(null, null, null);
+
+            // Fake HttpContext
+            var httpContext = new DefaultHttpContext();
+
+            model.PageContext = new PageContext()
+            {
+                HttpContext = httpContext
+            };
+
+            // Act
+            var result = await model.OnPostAddToCartAsync(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToPageResult));
+
+            var redirectResult = result as RedirectToPageResult;
+
+            Assert.AreEqual("/Login", redirectResult.PageName);
+        }
     }
 }
